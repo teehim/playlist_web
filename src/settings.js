39995eab531ea67e744ba020200b1234e612657e
teehim <teehim.thanatkorn@gmail.com>
2020-10-29 15:39:22 +0700

    update redirect url

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,2 @@
 export const clientId = 'ca83153c363a4fbc8fc683647b84831d';
-export const redirectUri = 'https://19767e2dac63.ngrok.io/home';
+export const redirectUri = 'https://playlist-clustering.herokuapp.com';
