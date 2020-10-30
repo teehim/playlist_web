f64b94a31fa903eb85f25daf3fabbe2a8e844bad
teehim <teehim.thanatkorn@gmail.com>
2020-10-30 17:28:18 +0700

    added action tab

--- src/home.js
+++ src/home.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import { Box, Grid, Typography, List, Avatar, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import { 
+    Box, 
+    Grid, 
+    Typography, 
+    List, 
+    Avatar, 
+    ListItem, 
+    ListItemAvatar, 
+    ListItemText, 
+    ListItemSecondaryAction,
+    IconButton
+} 
+from '@material-ui/core';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import GroupWorkOutlinedIcon from '@material-ui/icons/GroupWorkOutlined';
 import { withStyles } from '@material-ui/core/styles';
 import { withRouter } from "react-router-dom";
 
@@ -37,6 +51,13 @@ const useStyles = theme => ({
         height: '50px',
         margin: '0px 15px 0px 0px'
     },
+    action_item: {
+        'padding-left': '0px',
+        'margin-bottom': '10px'
+    },
+    actionlist: {
+        'margin-top': '10px'
+    }
 })
 
 class Home extends React.Component {
@@ -65,14 +86,18 @@ class Home extends React.Component {
                             </Typography>
                         </Box>
                     </Grid>
-                    <Grid item xs={12} className={classes.playlist_grid}>
+                    <Grid item xs={3} className={classes.playlist_grid}>
                         <Typography variant="h4">
                             Playlists
                         </Typography>
                         <List className={classes.playlist}>
                             {this.state.user? this.state.user.playlists.map((playlist) => {
                                 return (
-                                    <ListItem className={classes.playlist_item} button>
+                                    <ListItem 
+                                        className={classes.playlist_item}
+                                        onClick={this.setState({ playlist: playlist })}
+                                        button
+                                    >
                                         <ListItemAvatar>
                                             <Avatar
                                                 src={playlist.images[0].url}
@@ -81,11 +106,33 @@ class Home extends React.Component {
                                             />
                                         </ListItemAvatar>
                                         <ListItemText primary={playlist.name} secondary={playlist.track_counts + ' Songs'} />
+                                        <ListItemSecondaryAction>
+                                            <ListItemSecondaryAction>
+                                                <IconButton>
+                                                    <ArrowForwardIosIcon/>
+                                                </IconButton>
+                                            </ListItemSecondaryAction>
+                                        </ListItemSecondaryAction>
                                     </ListItem>
                                 );
                             }):''}
                         </List>
                     </Grid>
+                    {
+                        this.state.playlist?
+                        <Grid item xs={3}>
+                            <Typography variant="h4">
+                                {this.state.playlist.name}
+                            </Typography>
+                            <List className={classes.actionlist}>
+                                <ListItem className={classes.action_item} button>
+                                    <GroupWorkOutlinedIcon style={{ fontSize: 35, 'margin-right': 10 }} />
+                                    <ListItemText primary='Cluster Playlist'/>
+                                </ListItem>
+                            </List>
+                        </Grid>
+                        :''
+                    }
                 </Grid>
             </div>
         );
