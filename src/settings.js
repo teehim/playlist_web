b2cfc885af186a1cca03e0e30179562108bf0910
teehim <teehim.thanatkorn@gmail.com>
2020-10-30 12:31:46 +0700

    update server url

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export const clientId = 'ca83153c363a4fbc8fc683647b84831d';
 export const redirectUri = 'https://playlist-clustering.herokuapp.com';
-export const serverUrl = 'https://1c231399345c.ngrok.io';
+export const serverUrl = 'https://639d49c815b5.ngrok.io';
