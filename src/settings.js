59cd635192ae471b6ce285dda76add3b8637cf00
teehim <teehim.thanatkorn@gmail.com>
2020-11-18 23:23:27 +0700

    change to https

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export const clientId = 'ca83153c363a4fbc8fc683647b84831d';
 export const redirectUri = 'https://playlist-clustering.herokuapp.com';
-export const serverUrl = 'http://395ad813d0a1.ngrok.io';
+export const serverUrl = 'https://395ad813d0a1.ngrok.io';
