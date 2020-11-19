d17d0f9c2f747f58383c4cad2d22fc4521df0941
teehim <teehim.thanatkorn@gmail.com>
2020-11-20 04:55:31 +0700

    update server url to heroku url

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export const clientId = 'ca83153c363a4fbc8fc683647b84831d';
 export const redirectUri = 'https://playlist-clustering.herokuapp.com';
-export const serverUrl = 'https://b25675035d72.ngrok.io';
+export const serverUrl = 'https://playlist-clustering-backend.herokuapp.com';
