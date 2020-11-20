26888a2471691dd728d3b9012f2ac68c250fe2ee
teehim <teehim.thanatkorn@gmail.com>
2020-11-20 12:17:45 +0700

    Revert "debug servel url"
    This reverts commit f83ff3a172b6a326b25b183473c72ddc96a4e970.

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export const clientId = 'ca83153c363a4fbc8fc683647b84831d';
 export const redirectUri = 'https://playlist-clustering.herokuapp.com';
-export const serverUrl = 'https://f3a653f120c9.ngrok.io';
+export const serverUrl = 'https://playlist-clustering-backend.herokuapp.com';
